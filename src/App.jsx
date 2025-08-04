735e9533d35457a5e6eeda30c018b40c5f1baefd
kayahan <kayahanaakhunji@gmail.com>
2025-08-04 11:29:06 +0530

    bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import Intro from "./components/Intro";
 import Info from "./components/Info";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
+  window.addEventListener("load", () => {
+    ScrollTrigger.refresh();
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      ScrollTrigger.refresh();
+    }, 500);
+  }, []);
+
   return (
     <main>
       <Intro />
